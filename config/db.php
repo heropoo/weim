fe5722e4d74e62d0455742d32e611895de913adb
heropoo <aiyouyou1000@163.com>
2018-07-29 01:44:27 +0800

    dnb

--- config/db.php
+++ config/db.php
@@ -8,9 +8,9 @@
 
 return [
     'class'=>\Moon\Db\Connection::class,
-    'dsn' => 'mysql:host=localhost;dbname=weim;port=3306',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host='.env('DB_HOST', 'localhost').';dbname=weim;port=3306',
+    'username' => env('DB_USER','root'),
+    'password' => env('DB_PWD', ''),
     'charset' => 'utf8mb4',
     'tablePrefix' => 'we_',
     'emulatePrepares' => false,
