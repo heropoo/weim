b24160eba78fce41095af45cbb001d3a98a0fafb
heropoo <aiyouyou1000@163.com>
2019-07-09 19:06:52 +0800

    config

--- config/db.php
+++ config/db.php
@@ -7,9 +7,10 @@
  */
 
 return [
-    'class'=>\Moon\Db\Connection::class,
-    'dsn' => 'mysql:host='.env('DB_HOST', 'localhost').';dbname=weim;port=3306',
-    'username' => env('DB_USER','root'),
+    'class' => Moon\Db\Connection::class,
+    'dsn' => 'mysql:host=' . env('DB_HOST', 'localhost') . ';dbname=' . env('DB_NAME', 'weim')
+        . ';port=' . env('DB_PORT', '3306'),
+    'username' => env('DB_USER', 'root'),
     'password' => env('DB_PWD', ''),
     'charset' => 'utf8mb4',
     'tablePrefix' => 'we_',
